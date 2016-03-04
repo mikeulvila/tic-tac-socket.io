d0b5dac4d612d0c9e56b6b2e4f942dad1129aed1
Mike Ulvila <mikeulvila@me.com>
2016-03-04 12:33:21 -0600

    clicking to add x

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,12 @@
 
   socket.on('connect', () => {
     console.log('client socket connected')
-  })
+  });
+
+  $(".box").click(function () {
+    console.log($(this));
+    $(this).append( "<i class='fa fa-times fa-4x'></i>" );
+  });
 
 
 
