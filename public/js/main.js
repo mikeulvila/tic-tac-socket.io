f5b2ed662484c63d0dc4a16cd3d1626abb5dead5
Mike Ulvila <mikeulvila@me.com>
2016-03-04 14:01:02 -0600

    alternating x and o

--- public/js/main.js
+++ public/js/main.js
@@ -11,11 +11,19 @@
   let playerMoves = 0;
 
   $(".box").click(function () {
+    // check to see if x or o font already exists
     if ($(this).find('i').length === 0) {
-      console.log('no child, add that x dog');
-      $(this).append( "<i class='fa fa-times fa-4x'></i>" );
+      // alternate x and o
+      if (playerMoves%2 === 0) {
+        $(this).append( "<i class='fa fa-times fa-4x'></i>" );
+        playerMoves++
+      } else {
+        $(this).append( "<i class='fa fa-circle-o fa-4x'></i>" );
+        playerMoves++
+      }
+    // alert if div already taken
     } else {
-      console.log('already has a child bitch');
+      alert("You can't go there dude, already takin!");
     }
   });
 
